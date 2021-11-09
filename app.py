762139496147c92a33e3c7a18c8e618323bda80f
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2021-11-09 03:59:53 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,177 @@
+from class_funct import *
+
+
+@app.route('/users/', methods=['GET', 'POST'])
+def all_users():
+    """GET-запросы получения всех пользователей.
+    Создание пользователя посредством метода POST"""
+
+    if request.method == 'GET':
+        all_user = db.session.query(Users).all()
+        result = []
+        for u in all_user:
+            result.append(u.user_dict())
+        return jsonify(result)
+
+    elif request.method == 'POST':
+        data_user = json.load(request.get_json())
+        new_user = Users(
+            first_name=data_user['first_name'],
+            last_name=data_user['last_name'],
+            age=data_user['age'],
+            email=data_user['email'],
+            role=data_user['role'],
+            phone=data_user['phone']
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return "", 201
+
+
+@app.route('/users/<int:uid>', methods=['GET', 'PUT', 'DELETE'])
+def one_users(uid: int):
+    """Обрабатывает GET-запросы получения пользователя по идентификатору
+    Обновление пользователя посредством метода PUT.
+    Удаление пользователя посредством метода DELETE"""
+
+    if request.method == 'GET':
+        all_user = db.session.query(Users).all()
+        for u in all_user:
+            if u.id == uid:
+                return jsonify(u.user_dict())
+
+    elif request.method == 'DELETE':
+        delete_user = Users.query.get(uid)
+        db.session.delete(delete_user)
+        db.session.commit()
+        return jsonify(""), 204
+
+    elif request.method == 'PUT':
+        data_user = json.load(request.get_json())
+        change_user = Users.query.get(uid)
+        change_user.first_name = data_user['first_name']
+        change_user.last_name = data_user['last_name']
+        change_user.age = data_user['age']
+        change_user.email = data_user['email']
+        change_user.role = data_user['role']
+        change_user.phone = data_user['phone']
+        db.session.add(change_user)
+        db.session.commit()
+        return jsonify(""), 204
+
+
+@app.route('/orders/', methods=['GET', 'POST'])
+def all_orders():
+    """GET-запросы получения всех заказов.
+    Создание заказа order посредством метода POST на URL /orders"""
+
+    if request.method == 'GET':
+        all_orders = db.session.query(Order).all()
+        result = []
+        for order in all_orders:
+            result.append(order.order_dict())
+        return jsonify(result)
+
+    elif request.method == 'POST':
+        data_order = json.load(request.get_json())
+        new_order = Order(
+            name=data_order['name'],
+            description=data_order['description'],
+            start_date=data_order['start_date'],
+            end_date=data_order['end_date'],
+            address=data_order['address'],
+            price=data_order['price'],
+            customer_id=data_order['customer_id'],
+            executor_id=data_order['executor_id']
+        )
+        db.session.add(new_order)
+        db.session.commit()
+        return "", 201
+
+
+@app.route('/orders/<int:uid>', methods=['GET', 'PUT', 'DELETE'])
+def one_order(uid: int):
+    """Обрабатывает GET-запросы получения заказа по идентификатору.
+    Обновление заказа посредством метода PUT на URL /orders/<id>.
+    Удаление заказа order посредством метода DELETE на URL /orders/<id>"""
+
+    if request.method == 'GET':
+        all_orders = db.session.query(Order).all()
+        for order in all_orders:
+            if order.id == uid:
+                return jsonify(order.order_dict())
+
+    elif request.method == 'DELETE':
+        delete_order = Users.query.get(uid)
+        db.session.delete(delete_order)
+        db.session.commit()
+        return jsonify(""), 204
+
+    elif request.method == 'PUT':
+        data_order = json.load(request.get_json())
+        change_order = Users.query.get(uid)
+        change_order.name = data_order['name']
+        change_order.description = data_order['description']
+        change_order.end_date = data_order['end_date']
+        change_order.address = data_order['address']
+        change_order.price = data_order['price']
+        change_order.customer_id = data_order['customer_id']
+        change_order.executor_id = data_order['executor_id']
+        db.session.add(change_order)
+        db.session.commit()
+        return jsonify(""), 204
+
+
+@app.route('/offers/', methods=['GET', 'POST'])
+def all_offers():
+    """GET-запросы получения всех предложений.
+     Создание предложения посредством метода POST"""
+
+    if request.method == 'GET':
+        all_offers = db.session.query(Offers).all()
+        result = []
+        for offer in all_offers:
+            result.append(offer.offer_dict())
+        return jsonify(result)
+
+    elif request.method == 'POST':
+        data_offer = json.load(request.get_json())
+        new_offer = Offers(
+            order_id=data_offer['order_id"'],
+            executor_id=data_offer['executor_id']
+        )
+        db.session.add(new_offer)
+        db.session.commit()
+        return "", 201
+
+
+@app.route('/offers/<int:uid>', methods=['GET', 'PUT', 'DELETE'])
+def one_offer(uid: int):
+    """Обрабатывает GET-запросы получения заказа по идентификатору.
+    Обновление предложения посредством метода PUT.
+    Удаление предложения посредством метода DELETE"""
+
+    if request.method == 'GET':
+        all_offers = db.session.query(Offers).all()
+        for offer in all_offers:
+            if offer.id == uid:
+                return jsonify(offer.offer_dict())
+
+    elif request.method == 'DELETE':
+        delete_offer = Users.query.get(uid)
+        db.session.delete(delete_offer)
+        db.session.commit()
+        return jsonify(""), 204
+
+    elif request.method == 'PUT':
+        data_offer = json.load(request.get_json())
+        change_offer = Users.query.get(uid)
+        change_offer.order_id = data_offer['order_id']
+        change_offer.executor_id_id = data_offer['executor_id']
+        db.session.add(change_offer)
+        db.session.commit()
+        return jsonify(""), 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ class_funct.py
@@ -0,0 +1,96 @@
+import json
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    email = db.Column(db.String)
+    role = db.Column(db.String)
+    phone = db.Column(db.String)
+
+    def user_dict(self):
+        return {"id": self.id,
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age,
+                "email": self.email,
+                "role": self.role,
+                "phone": self.phone,
+                }
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    start_date = db.Column(db.String)
+    end_date = db.Column(db.String)
+    address = db.Column(db.String)
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def order_dict(self):
+        return {"id": self.id,
+                "name": self.name,
+                "description": self.description,
+                "start_date": self.start_date,
+                "end_date": self.end_date,
+                "address": self.address,
+                "price": self.price,
+                "customer_id": self.customer_id,
+                "executor_id": self.executor_id,
+                }
+
+
+class Offers(db.Model):
+    __tablename__ = 'offers'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+    def offer_dict(self):
+        return {"id": self.id,
+                "order_id": self.order_id,
+                "executor_id": self.executor_id,
+                }
+
+
+db.drop_all()
+db.create_all()
+db.session.commit()
+
+
+with open("users.json", encoding='utf-8') as f:
+    users = json.load(f)
+for user in users:
+    db.session.add(Users(**user))
+db.session.commit()
+
+
+with open('orders.json', encoding='utf-8') as f:
+    orders = json.load(f)
+for order in orders:
+    db.session.add(Order(**order))
+db.session.commit()
+
+
+with open('offers.json', encoding='utf-8') as f:
+    offers = json.load(f)
+for offer in offers:
+    db.session.add(Offers(**offer))
+db.session.commit()
+
